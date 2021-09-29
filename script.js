9117d5cbb1e159d96cb8ea2a73ac2a658b1f29ee
arjunrvan <arjunrvan@gmail.com>
2021-09-29 16:37:41 +0800

    update features

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var newListHolder = document.getElementById('newlist-holder');
-
 var compListHolder = document.getElementById('complist-holder');
-
 var taskInput = document.getElementById('myInput');
 
 function addTask () {
@@ -39,15 +37,23 @@ function addTask () {
 
 function taskCheck () {
     var delBtn = document.createElement('button');
+    var resBtn = document.createElement('button');
+    
+
     delBtn.innerHTML = "Delete";
+    resBtn.innerHTML = "Restore";
 
     delBtn.classList.add('del-btn');
+    resBtn.classList.add('res-btn');
 
     var parentDiv = this.parentNode;
     parentDiv.remove();
 
     delBtn.onclick = taskDel;
+    resBtn.onclick = taskRes;
+    parentDiv.append(resBtn);
     parentDiv.append(delBtn);
+    
 
     parentDiv.querySelector('h3').style.textDecoration = 'line-through';
 
@@ -60,6 +66,32 @@ function taskCheck () {
 function taskDel () {
     var parentDiv = this.parentNode;
     parentDiv.remove();
+}
+
+function taskRes () {
+    var parentDiv = this.parentNode;
+    parentDiv.remove();
+    var editBtn = document.createElement('button');
+    editBtn.innerHTML = "Edit";
+
+    var taskBtn = document.createElement('button');
+    taskBtn.innerHTML = "Done";
+
+    editBtn.classList.add('task-btn');
+    taskBtn.classList.add('task-btn');
+
+    editBtn.onclick = taskEdit;
+    taskBtn.onclick = taskCheck;
+
+    parentDiv.append(editBtn);
+    parentDiv.append(taskBtn);
+        
+    newListHolder.append(parentDiv);
+    this.remove();
+    parentDiv.querySelectorAll('button')[0].remove();
+
+    parentDiv.querySelector('h3').style.textDecoration = 'none';
+    
 }
 
 var timing;
